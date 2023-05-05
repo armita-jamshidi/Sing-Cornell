ba20a7fe1611de1a331608662a41911c260bac48
selinalin300 <76505335+selinalin300@users.noreply.github.com>
2023-05-04 21:06:24 -0400

    Update db.py

--- src/db.py
+++ src/db.py
@@ -20,6 +20,9 @@ S3_BUCKET_NAME= os.environ.get("S3_BUCKET_NAME")
 S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com"
 
 class Asset(db.Model):
+    """
+    Image Model
+    """
     __tablename__="assets"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     base_url = db.Column(db.String, nullable=True)
@@ -110,7 +113,7 @@ class Songs(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    artistname =db.Column(db.String, nullable=False)
     song_link = db.Column(db.String, nullable = False)
     image = db.relationship("Asset", cascade ="delete")
 
@@ -121,10 +124,8 @@ class Songs(db.Model):
         """
         self.name = kwargs.get("name")
         self.description = kwargs.get("description")
-        self.user_id = kwargs.get("user_id")
+        self.artistname = kwargs.get("artistname")
         self.song_link = kwargs.get("song_link")
-        
-        #extracting image data
         
 
     def serialize(self):
@@ -136,45 +137,9 @@ class Songs(db.Model):
             "id": self.id,
             "name": self.name, 
             "description": self.description,
-            "user_id": self.user_id,
+            "artistname":self.artistname,
             "song_link":self.song_link,
-            #"image": [s.serialize() for s in self.image]
-            #aws/image.png
+            "image": [s.serialize() for s in self.image]
             
         }
     
-class User(db.Model):
-    """
-    User model
-    """
-
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    class_year = db.Column(db.String, nullable=False)
-    songs = db.relationship("Songs", cascade="delete")
-
-    def __init__(self, **kwargs):
-        """
-        Initializes a User object
-        """
-        self.name = kwargs.get("name")
-        self.class_year = kwargs.get("class_year")
-    
-    def get_songs(self):
-        """
-        Returns the songs for this User
-        """
-        songs= []
-        for s in self.songs:
-            songs.append(s)
-        return songs
-    
-    def serialize(self):
-        """
-        Serializes a User object
-        """
-        songs = self.get_songs()
-        return{"id": self.id, "name": self.name, "class_year": self.class_year, 
-               "songs": [s.serialize() for s in songs]}
-
